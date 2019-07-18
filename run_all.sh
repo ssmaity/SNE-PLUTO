8a28d78bfb5773b65a2c93e44bd3bb571aa4d3dc
Samriddhi Sankar Maity <ssmaity94@gmail.com>
2019-07-18 19:52:51 +0530

    Update run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -1,27 +1,31 @@
-echo 'Analysis module starting...'
+echo Analysis module starting...
 
 ./files.sh
 
-if [$? -eq 0]; then
-    echo 'Data files found. Going to next step...'
+if [ $? -eq 0 ]
+then
+    echo Data files found. Going to next step...
 else
-    echo 'Error: No data files exist!'
+    echo Error: No data files exist!
 fi
 
 python3 file_names.py > newfiles.dat
 
-echo 'Extracting data from files. Going to next step...'
+echo Extracting data from files. Going to next step...
 
 python3 extract_rdata.py > rdata.dat
-if [$? -eq 0]; then
-   echo 'Extraction of data from files successful. Going to next step...'
+if [ $? -eq 0 ]
+then
+   echo Extraction of data from files successful. Going to next step...
 else
-    echo 'Error: Problem in extracting data!'
+    echo Error: Problem in extracting data!
 fi
 
 python3 plot_numerical.py
-if [$? -eq 0]; then
-   echo 'Process completed. Image saved successfully!'
+if [ $? -eq 0 ]
+then
+   echo Process completed. Image saved successfully.
+   echo Leaving Analysis module!
 else
-    echo 'Error: Can't generate image!'
+    echo Error: Can\'t generate image!
 fi
